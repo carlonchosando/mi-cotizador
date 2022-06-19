a6cd0af74b275af9710075c3ea4491fff1e9fd74
carlonchosando <cdscapitolio@gmail.com>
2022-06-19 09:46:22 -0300

    años

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Formulario from './Formulario';
-
+import {obtenerDiferenciaAnio} from '../helper';
 
 
 class App extends Component {
 
   cotizarSeguro = (datos) => {  
-     console.log(datos);
+     const { marca, year, plan } = datos;
+
+    let resultado = 2000;
+
+    //obtener la diferencia de años
+    const diferencia = obtenerDiferenciaAnio(year);
+    
+  
+
+    //por cada añ restar el 3% al valor del seguro
+
+
+    //Americano 15% si es basico 20% si es completo
+
+
+
+
   }
 
 render() {
