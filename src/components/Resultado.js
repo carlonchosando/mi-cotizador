55f66165a3e462f76f353b85f6468b60f8f8ac8e
carlonchosando <cdscapitolio@gmail.com>
2022-06-19 20:22:35 -0300

    reparadoNoFuncaVersionDe

--- src/components/Resultado.js
+++ src/components/Resultado.js
@@ -1,30 +1,13 @@
 import React, { Component } from "react";
-import ReactDom, { render } from "react-dom";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
+
 
 class Resultado extends Component {
   render() {
-    const resultado = this.props.resultado;
-
-    const mensaje = (!resultado)
-      ? "Elije Marca, Año y tipo de seguro, Básico o Completo"
-      : "El total es de: $";
-
-    return (
-      <div className="gran-total">
-        
-        
-          <TransitionGroup component="span" className="resultado">
-            <CSSTransition
-              classNames="resultado"
-              key={resultado}
-              timeout={{ enter: 500, exit: 500 }}>
-              <span>{Resultado}</span>
-            </CSSTransition>
-          </TransitionGroup>
-        
-      </div>
-    );
+        return (
+             <p>$ {this.props.Resultado}</p>  
+            );
+    
   }
 }
 
